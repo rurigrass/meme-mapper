d11dae9ad578241c6e28454682fd7a0a79605b6c
rurigrass <ruripg@gmail.com>
2023-08-09 20:20:35 +0100

    registration works

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -5,11 +5,14 @@ import bcrypt from "bcrypt";
 export async function POST(req: Request) {
   try {
     const body = await req.json();
-    const { username, email, password } = body;
-    const session = await getAuthSession();
-    if (session?.user) {
-      return new Response("Already logged in", { status: 200 });
-    }
+    const { username, email, password } = body.data;
+    console.log(username);
+
+    //ADD THIS LATER - CHECK IF ALREADY LOGGED IN
+    // const session = await getAuthSession();
+    // if (session?.user) {
+    //   return new Response("Already logged in", { status: 200 });
+    // }
 
     // CHECK REQUEST IS VALID - could add zod validation here
     if (!username || !email || !password) {
@@ -27,6 +30,7 @@ export async function POST(req: Request) {
 
     // HASH PASSWORD
     const hashedPassword = await bcrypt.hash(password, 10);
+    console.log("HELLO");
 
     //CREATE THE USER
     const user = await db.user.create({
@@ -37,10 +41,12 @@ export async function POST(req: Request) {
       },
     });
 
+    console.log("USER", user);
+
     return new Response(JSON.stringify(user));
   } catch (error) {
     console.log(error);
 
-    return new Response("Could not add company", { status: 500 });
+    return new Response("Could not add user", { status: 500 });
   }
 }
