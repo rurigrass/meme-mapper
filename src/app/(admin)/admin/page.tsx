8d31d7c3be318102f9cf54d47c7b16ae0ebee2e8
rurigrass <ruripg@gmail.com>
2023-08-22 12:28:23 +0100

    start of admin table

--- src/app/(admin)/admin/page.tsx
+++ src/app/(admin)/admin/page.tsx
@@ -1,21 +1,25 @@
-"use client";
+// "use client";
 
+import MemeTable from "@/components/admin/MemeTable";
 import { Button } from "@/components/ui/button";
+import { db } from "@/lib/db";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { FC } from "react";
 
 interface pageProps {}
 
-const Page: FC<pageProps> = ({}) => {
-  const { data: session, status, update } = useSession();
-  console.log(session);
+const Page = async () => {
+  // const { data: session, status, update } = useSession();
+  // console.log(session);
+  const memes = await db.meme.findMany({});
 
   return (
     <div className="">
       <Link href={"/admin/add-meme"}>
         <Button>Add New Meme</Button>
       </Link>
+      <MemeTable memes={memes} />
     </div>
   );
 };
