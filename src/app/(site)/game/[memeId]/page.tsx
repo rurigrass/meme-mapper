19571e3bb3d46f6b58c649f51bc855e90f5bff7e
rurigrass <ruripg@gmail.com>
2023-08-29 11:13:21 +0100

    added scores to prisma

---
+++ src/app/(site)/game/[memeId]/page.tsx
@@ -0,0 +1,21 @@
+import { db } from "@/lib/db";
+
+interface PageProps {
+  params: {
+    memeId: string;
+  };
+}
+
+const Page = async ({ params }: PageProps) => {
+  const { memeId } = params;
+
+  const meme = await db.meme.findFirst({
+    where: {
+      id: memeId,
+    },
+  });
+
+  return <div>page</div>;
+};
+
+export default Page;
