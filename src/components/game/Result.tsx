f751c689f7e3f07fc94bd6def595c56edef84bab
rurigrass <ruripg@gmail.com>
2023-09-09 21:25:27 +0100

    some rounded corners on the result map

--- src/components/game/Result.tsx
+++ src/components/game/Result.tsx
@@ -22,12 +22,12 @@ const Result = ({
   console.log("DISTANCE", distance);
 
   return (
-    <div className="relative h-full overflow-hidden">
+    <div className="relative h-full overflow-hidden rounded-lg">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10">
         {/* <Card className="w-72 h-52">{distance}</Card> */}
         <div>You were {distance.toFixed(2)}Km away</div>
       </div>
-      <div className="inset-0 absolute brightness-50">
+      <div className="h-full brightness-50">
         <ResultMap
           actualCoordinates={actualCoordinates}
           guessCoordinates={guessCoordinates}
