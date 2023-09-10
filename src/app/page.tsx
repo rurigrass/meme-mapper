9fb29eef53810b39e5f0e42723ad96c6a625ae4c
rurigrass <ruripg@gmail.com>
2023-09-10 22:34:35 +0100

    just try this

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,25 +6,43 @@ import Text3d from "@/components/home/Text3d";
 import { Button } from "@/components/ui/button";
 import { getAuthSession } from "@/lib/auth";
 import Link from "next/link";
+import { useRef } from "react";
 
 export default async function Home() {
+  const plane = useRef<HTMLDivElement | null>(null);
+  const maxRotate = 45;
   // const session = await getAuthSession();
-  // const manageMouseMove = (e: any) => {
-  //   console.log(e);
-  // };
+  const manageMouseMove = (e: any) => {
+    const x = e.clientX / window.innerWidth;
+    const y = e.clientY / window.innerHeight;
+    const perspective = window.innerWidth * 4;
+    const rotateX = maxRotate * x - maxRotate / 2;
+    const rotateY = (maxRotate * y - maxRotate / 2) * -1;
+    if (plane.current) {
+      plane.current.style.transform = `perspective(${perspective}px) rotateX(${rotateY}deg) rotateY(${rotateX}deg)`;
+    }
+  };
 
   return (
     <main
       className="relative flex-1 align-middle justify-center h-full"
-      onMouseMove={() => {
-        // manageMouseMove(e);
-        console.log("MOVEMENT yeah");
+      onMouseMove={(e) => {
+        manageMouseMove(e);
       }}
     >
       {/* <pre>{JSON.stringify(session)}</pre> */}
       {/* BODY */}
-      <div className="flex flex-col justify-start items-start gap-3 pt-10 absolute container z-10">
+      {/* <div
+       
+      > */}
+      <div
+        ref={plane}
+        className="flex flex-col justify-start items-start gap-3 pt-10 absolute container z-10"
+      >
         {/* <code>{JSON.stringify(process.env.GOOGLE_MAPS_API_KEY)}</code> */}
+        {/* <div className=" text-[8vw]">
+            HELLO
+          </div> */}
         <Link href={"/login"}>
           <Text3d primary="Login" secondary="Login" />
         </Link>
@@ -35,6 +53,7 @@ export default async function Home() {
           <Text3d primary="Admin" secondary="Admin" />
         </Link>
       </div>
+      {/* </div> */}
       <Globe />
     </main>
   );
