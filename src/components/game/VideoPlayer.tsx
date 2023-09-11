1c9914db078aeecc0b384dc41e31d0ee373899e2
rurigrass <ruripg@gmail.com>
2023-09-11 18:55:33 +0100

    silly framer motion animation

--- src/components/game/VideoPlayer.tsx
+++ src/components/game/VideoPlayer.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import { motion, spring } from "framer-motion";
 
 interface VideoPlayerProps {
   fileUrl: string;
@@ -18,16 +19,23 @@ const VideoPlayer = ({ fileUrl }: VideoPlayerProps) => {
 
   return (
     <div className={`relative`}>
-      {muted && (
-        <div
-          onClick={() => setMuted(false)}
-          className="absolute max-w-full bg-black opacity-50 inset-0 m-auto rounded-md "
-        >
-          <div className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 text-[4.6vw] font-bold text-white opacity-70 mix-blend-screen break-normal ">
-            Click for Sound
-          </div>
+      {/* {muted && ( */}
+      <motion.div
+        key="animation-on-muted"
+        onClick={() => setMuted(false)}
+        className="absolute max-w-full inset-0 m-auto rounded-md "
+        animate={
+          muted
+            ? { opacity: 0.5, backgroundColor: "black", x: 0 }
+            : { opacity: 0, x: "100vw" }
+        }
+        transition={{ type: "spring", delay: 0.2 }}
+      >
+        <div className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 text-[4.6vw] font-bold text-white opacity-70 mix-blend-screen break-normal ">
+          Click for Sound
         </div>
-      )}
+      </motion.div>
+      {/* )} */}
       <video
         className={`rounded-md w-full ${muted && "hover:cursor-pointer"} `}
         autoPlay
