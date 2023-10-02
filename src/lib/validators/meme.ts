56cfcacfc67be37753adc8328c9f511f57dddbaa
rurigrass <ruripg@gmail.com>
2023-10-02 15:46:36 +0100

    zod validation on the map pin

--- src/lib/validators/meme.ts
+++ src/lib/validators/meme.ts
@@ -39,8 +39,34 @@ export const MemeValidator = z.object({
   //   `Max file size is 5MB.`
   // )
   latlng: z.object({
-    lat: z.number(),
-    lng: z.number(),
+    lat: z
+      .number()
+      .min(0.0000001, { message: "Latitude must be greater than 0" })
+      .max(90, { message: "Latitude must be less than or equal to 90" })
+      .or(
+        z
+          .number()
+          .min(-90, {
+            message: "Latitude must be greater than or equal to -90",
+          })
+          .max(-0.0000001, {
+            message: "Latitude must be less than 0",
+          })
+      ),
+    lng: z
+      .number()
+      .min(0.0000001, { message: "Longitude must be greater than 0" })
+      .max(180, { message: "Longitude must be less than or equal to 180" })
+      .or(
+        z
+          .number()
+          .min(-180, {
+            message: "Longitude must be greater than or equal to -180",
+          })
+          .max(-0.0000001, {
+            message: "Longitude must be less than 0",
+          })
+      ),
   }),
   // lat: number(),
   // lng: number(),
