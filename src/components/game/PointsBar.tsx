54c7dc0768d628cdf6345d694786e05e81934fe0
rurigrass <ruripg@gmail.com>
2023-10-05 18:12:23 +0100

    last try this transition ting

--- src/components/game/PointsBar.tsx
+++ src/components/game/PointsBar.tsx
@@ -10,13 +10,19 @@ const PointsBar = ({ points }: PointBarProps) => {
   const widthPercentage = (points / 5000) * 100;
   console.log(widthPercentage);
 
+  const transition: Transition = {
+    type: "tween",
+    damping: 20,
+    duration: 1,
+  };
+
   return (
     <div className="relative w-[75vw] md:w-[100%] h-6 flex justify-center items-center bg-yellow-500 rounded-full">
       <motion.div
         className="absolute left-0 rounded-l-full bg-purple-600 h-full"
         initial={{ width: 0 }}
         animate={{ width: `${widthPercentage}%` }}
-        transition={{ type: "tween", damping: 20, duration: 1 }}
+        transition={transition}
       />
       <div className="text-white z-10">
         <Counter distance={points} />
