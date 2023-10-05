f9d8d0c721aeada731c21019c61e236a49c65e73
rurigrass <ruripg@gmail.com>
2023-10-05 18:04:22 +0100

    again without type transition

--- src/components/game/PointsBar.tsx
+++ src/components/game/PointsBar.tsx
@@ -10,12 +10,11 @@ const PointsBar = ({ points }: PointBarProps) => {
   const widthPercentage = (points / 5000) * 100;
   console.log(widthPercentage);
 
-  const transition: Transition = {
-    type: "just",
-    // stiffness: { type: "number", value: 260 },
-    damping: 20,
-    duration: 1,
-  };
+  // const transition: Transition = {
+  //   type: "just",
+  //   damping: 20,
+  //   duration: 1,
+  // };
 
   return (
     <div className="relative w-[75vw] md:w-[100%] h-6 flex justify-center items-center bg-yellow-500 rounded-full">
@@ -23,7 +22,7 @@ const PointsBar = ({ points }: PointBarProps) => {
         className="absolute left-0 rounded-l-full bg-purple-600 h-full"
         initial={{ width: 0 }}
         animate={{ width: `${widthPercentage}%` }}
-        transition={transition}
+        transition={{ type: "just", damping: 20, duration: 1 }}
       />
       <div className="text-white z-10">
         <Counter distance={points} />
