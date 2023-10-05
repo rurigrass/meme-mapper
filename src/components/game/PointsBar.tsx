03c5cd281a09d5539f9dbb2f98b484584c35df75
rurigrass <ruripg@gmail.com>
2023-10-05 18:09:29 +0100

    try with tween transition

--- src/components/game/PointsBar.tsx
+++ src/components/game/PointsBar.tsx
@@ -10,19 +10,13 @@ const PointsBar = ({ points }: PointBarProps) => {
   const widthPercentage = (points / 5000) * 100;
   console.log(widthPercentage);
 
-  // const transition: Transition = {
-  //   type: "just",
-  //   damping: 20,
-  //   duration: 1,
-  // };
-
   return (
     <div className="relative w-[75vw] md:w-[100%] h-6 flex justify-center items-center bg-yellow-500 rounded-full">
       <motion.div
         className="absolute left-0 rounded-l-full bg-purple-600 h-full"
         initial={{ width: 0 }}
         animate={{ width: `${widthPercentage}%` }}
-        transition={{ type: "just", damping: 20, duration: 1 }}
+        transition={{ type: "tween", damping: 20, duration: 1 }}
       />
       <div className="text-white z-10">
         <Counter distance={points} />
