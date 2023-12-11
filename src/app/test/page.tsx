58e533c74a8f992871ad7dde980d3b7a18c07af9
rurigrass <ruripg@gmail.com>
2023-12-11 12:44:59 +0100

    change dec 11

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -3,9 +3,7 @@ import AppleMap from "@/components/game/AppleMap";
 const Page = ({}) => {
   const token = process.env.MAPKIT_TOKEN;
 
-  return (
-    <div className=" h-80 w-80">{token && <AppleMap token={token} />}</div>
-  );
+  return <div className="h-80 w-80">{token && <AppleMap token={token} />}</div>;
 };
 
 export default Page;
