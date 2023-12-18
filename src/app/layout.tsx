33897da16267d17cf2111a5169591fc22d080c01
rurigrass <ruripg@gmail.com>
2023-12-18 14:13:24 +0100

    [frontend] tried absolute inset 0

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,8 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning={true}>
       <body className={`${inter.className}`}>
         <Providers>
-          <div className="flex flex-col mx-auto  h-[calc(100dvh)]">
+          {/* h-[calc(100dvh)] */}
+          <div className="flex flex-col mx-auto absolute inset-0 ">
             <Navbar />
             {/* <div className="relative flex-1 overflow-hidden"> */}
             {/* usually classname has container for the side margins. and pt-20 */}
