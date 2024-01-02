216526b0049be81e11901941cc68184732d7833c
rurigrass <ruripg@gmail.com>
2024-01-02 12:52:08 +0000

    [frontend] removed center from map.

--- src/components/game/AppleMap.tsx
+++ src/components/game/AppleMap.tsx
@@ -31,49 +31,45 @@ const AppleMap = ({ token, updateCoordinates, mapTypeId }: AppleMapProps) => {
       updateCoordinates(guessMarker?.lat ?? 0, guessMarker?.lng ?? 0);
   }, [guessMarker]);
 
-  const center = {
-    centerLatitude: 40.4168,
-    centerLongitude: 3.7038,
-    latitudeDelta: 180 as number,
-    longitudeDelta: 90 as number,
-  };
+  // const center = {
+  //   centerLatitude: 40.4168,
+  //   centerLongitude: 3.7038,
+  //   latitudeDelta: 180 as number,
+  //   longitudeDelta: 90 as number,
+  // };
 
   return (
-    <>
-      {center && (
-        <Map
-          token={token}
-          initialRegion={center}
-          allowWheelToZoom
-          colorScheme={theme === "light" ? ColorScheme.Light : ColorScheme.Dark}
-          showsZoomControl={false}
-          showsCompass={FeatureVisibility.Hidden}
-          showsMapTypeControl={false}
-          mapType={mapTypeId}
-          // excludedPOICategories={[PointOfInterestCategory.Airport]}
+    <Map
+      token={token}
+      // initialRegion={center}
+      allowWheelToZoom
+      colorScheme={theme === "light" ? ColorScheme.Light : ColorScheme.Dark}
+      showsZoomControl={false}
+      showsCompass={FeatureVisibility.Hidden}
+      showsMapTypeControl={false}
+      mapType={mapTypeId}
+      // excludedPOICategories={[PointOfInterestCategory.Airport]}
 
-          onSingleTap={(e) => {
-            setGuessMarker({
-              lat: e.toCoordinates().latitude || 0,
-              lng: e.toCoordinates().longitude || 0,
-            }),
-              // marker.onChange(e.latLng);
-              updateCoordinates &&
-                updateCoordinates(
-                  e.toCoordinates().latitude ?? 0,
-                  e.toCoordinates().longitude ?? 0
-                );
-          }}
-        >
-          {guessMarker && (
-            <Marker
-              latitude={guessMarker.lat || 0}
-              longitude={guessMarker.lng || 0}
-            />
-          )}
-        </Map>
+      onSingleTap={(e) => {
+        setGuessMarker({
+          lat: e.toCoordinates().latitude || 0,
+          lng: e.toCoordinates().longitude || 0,
+        }),
+          // marker.onChange(e.latLng);
+          updateCoordinates &&
+            updateCoordinates(
+              e.toCoordinates().latitude ?? 0,
+              e.toCoordinates().longitude ?? 0
+            );
+      }}
+    >
+      {guessMarker && (
+        <Marker
+          latitude={guessMarker.lat || 0}
+          longitude={guessMarker.lng || 0}
+        />
       )}
-    </>
+    </Map>
   );
 };
 
