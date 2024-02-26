fe4651acb243eb41c01edf4349d274e314526cb2
rurigrass <ruripg@gmail.com>
2024-02-26 20:56:08 +0100

    page capital error deployu

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -3,7 +3,7 @@
 import LoadingScreen from "@/components/canvas/LoadingScreen";
 import { useState } from "react";
 
-const page = ({}) => {
+const Page = ({}) => {
   const [loadingEnded, setLoadingEnded] = useState<boolean>(false);
   return (
     <LoadingScreen
@@ -13,4 +13,4 @@ const page = ({}) => {
   );
 };
 
-export default page;
+export default Page;
