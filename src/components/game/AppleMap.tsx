24e97563513003c72dcf332f730d29ea6bde03c1
rurigrass <ruripg@gmail.com>
2024-03-17 22:22:06 +0100

    try this

--- src/components/game/AppleMap.tsx
+++ src/components/game/AppleMap.tsx
@@ -38,37 +38,41 @@ const AppleMap = ({ token, updateCoordinates, mapTypeId }: AppleMapProps) => {
   }, [guessMarker]);
 
   return (
-    <Map
-      token={token}
-      initialRegion={center}
-      allowWheelToZoom
-      colorScheme={theme === "light" ? ColorScheme.Light : ColorScheme.Dark}
-      showsZoomControl={false}
-      showsCompass={FeatureVisibility.Hidden}
-      showsMapTypeControl={false}
-      mapType={mapTypeId}
-      // excludedPOICategories={[PointOfInterestCategory.Airport]}
+    <>
+      {center && (
+        <Map
+          token={token}
+          initialRegion={center}
+          allowWheelToZoom
+          colorScheme={theme === "light" ? ColorScheme.Light : ColorScheme.Dark}
+          showsZoomControl={false}
+          showsCompass={FeatureVisibility.Hidden}
+          showsMapTypeControl={false}
+          mapType={mapTypeId}
+          // excludedPOICategories={[PointOfInterestCategory.Airport]}
 
-      onSingleTap={(e) => {
-        setGuessMarker({
-          lat: e.toCoordinates().latitude || 0,
-          lng: e.toCoordinates().longitude || 0,
-        }),
-          // marker.onChange(e.latLng);
-          updateCoordinates &&
-            updateCoordinates(
-              e.toCoordinates().latitude ?? 0,
-              e.toCoordinates().longitude ?? 0
-            );
-      }}
-    >
-      {guessMarker && (
-        <Marker
-          latitude={guessMarker.lat || 0}
-          longitude={guessMarker.lng || 0}
-        />
+          onSingleTap={(e) => {
+            setGuessMarker({
+              lat: e.toCoordinates().latitude || 0,
+              lng: e.toCoordinates().longitude || 0,
+            }),
+              // marker.onChange(e.latLng);
+              updateCoordinates &&
+                updateCoordinates(
+                  e.toCoordinates().latitude ?? 0,
+                  e.toCoordinates().longitude ?? 0
+                );
+          }}
+        >
+          {guessMarker && (
+            <Marker
+              latitude={guessMarker.lat || 0}
+              longitude={guessMarker.lng || 0}
+            />
+          )}
+        </Map>
       )}
-    </Map>
+    </>
   );
 };
 
