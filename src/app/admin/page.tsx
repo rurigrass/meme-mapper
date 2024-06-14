f04acae138a45eb33a41f3bd4e8850962dfdb0f9
rurigrass <ruripg@gmail.com>
2024-06-14 18:16:26 +0200

    tried a temp fix on the type with no |

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,15 +2,29 @@ import MemesTable from "@/components/admin/MemeTable";
 import { columns } from "@/components/admin/columns";
 import { Button } from "@/components/ui/button";
 import { db } from "@/lib/db";
-import { memeType } from "@/lib/types";
+import { MemeStatusTypes } from "@/lib/types";
 import Link from "next/link";
 
-interface pageProps {}
+export type memeTypeWithLatLng = {
+  id: string;
+  name: string;
+  description: string;
+  url: string;
+  fileUrl: string;
+  screenshotUrl: string;
+  lat: number;
+  lng: number;
+  verified: boolean;
+  status: MemeStatusTypes;
+  createdAt: Date;
+  updatedAt: Date;
+  creatorId: string | null;
+};
 
 const Page = async () => {
   const memes = await db.meme.findMany();
 
-  return <MemesTable columns={columns} data={memes as memeType[]} />;
+  return <MemesTable columns={columns} data={memes as memeTypeWithLatLng[]} />;
 };
 
 export default Page;
