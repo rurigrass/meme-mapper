dec6e9a33d6f8fa8015239e7ce5bb6fd7e7dffcb
rurigrass <ruripg@gmail.com>
2024-06-19 16:15:56 +0200

    pulling all detective memes in detective page

--- src/app/(site)/detective/page.tsx
+++ src/app/(site)/detective/page.tsx
@@ -1,6 +1,12 @@
 import { db } from "@/lib/db";
 
 const Page = async () => {
+  const memes = await db.meme.findMany({
+    where: { status: { equals: "DETECTIVE" } },
+  });
+
+  console.log(memes);
+
   return <div>DETECTIVE 🕵️‍♂️</div>;
 };
 
