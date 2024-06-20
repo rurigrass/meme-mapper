c1966c922c7db699bce0eb24a00d54b16255af19
rurigrass <ruripg@gmail.com>
2024-06-20 18:00:20 +0200

    added some pagination prisma

--- src/app/(site)/detective/page.tsx
+++ src/app/(site)/detective/page.tsx
@@ -1,8 +1,21 @@
 import { db } from "@/lib/db";
 
-const Page = async () => {
+export type PageProps = {
+  params: { [key: string]: string | string };
+};
+
+const Page = async (props: PageProps) => {
+  const take = 3;
+  const skip = 0;
   const memes = await db.meme.findMany({
+    take,
+    skip,
     where: { status: { equals: "DETECTIVE" } },
+    select: {
+      id: true,
+      name: true,
+    },
+    orderBy: { createdAt: "desc" },
   });
 
   console.log(memes);
---
+++ src/components/detective/DetectiveMemes.tsx
@@ -0,0 +1,3 @@
+const DetectiveMemes = () => {
+  return <div></div>;
+};
