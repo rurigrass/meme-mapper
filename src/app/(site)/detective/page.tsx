f3bae60e4a27e5d2b282db99eb218140710ac097
rurigrass <ruripg@gmail.com>
2024-06-21 22:53:54 +0200

    debating whether to use server actions

--- src/app/(site)/detective/page.tsx
+++ src/app/(site)/detective/page.tsx
@@ -1,24 +1,36 @@
 import { db } from "@/lib/db";
+import { useQuery } from "@tanstack/react-query";
 
 export type PageProps = {
   params: { [key: string]: string | string };
 };
 
-const Page = async (props: PageProps) => {
+const Page = (props: PageProps) => {
   const take = 3;
   const skip = 0;
-  const memes = await db.meme.findMany({
-    take,
-    skip,
-    where: { status: { equals: "DETECTIVE" } },
-    select: {
-      id: true,
-      name: true,
-    },
-    orderBy: { createdAt: "desc" },
-  });
 
-  console.log(memes);
+  // const {} = useQuery
+
+  const getMemes = async () => {
+    "use server";
+    const memes = await db.meme.findMany({
+      take,
+      skip,
+      where: { status: { equals: "DETECTIVE" } },
+      select: {
+        id: true,
+        name: true,
+      },
+      orderBy: { createdAt: "desc" },
+    });
+
+    console.log(memes);
+
+    return {
+      data: memes,
+      metadata: {},
+    };
+  };
 
   return <div>DETECTIVE 🕵️‍♂️</div>;
 };
