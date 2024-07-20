f3432c87c00ff3fccee02c850a76fce8e51a58ff
rurigrass <ruripg@gmail.com>
2024-07-20 11:58:02 +0200

    removed page from meme game layout

--- src/app/(site)/game/layout.tsx
+++ src/app/(site)/game/layout.tsx
@@ -1,25 +1,3 @@
-// import { getAuthSession } from "@/lib/auth";
-// import { db } from "@/lib/db";
-
-// const Page = async () => {
-//   const session = await getAuthSession();
-
-//   const user = await db.user.findFirst({
-//     where: {
-//       id: session?.user.id,
-//     },
-//   });
-
-//   return (
-//     <div className="pt-20">
-//       page
-//       {/* <pre>{JSON.stringify(session)}</pre> */}
-//     </div>
-//   );
-// };
-
-// export default Page;
-
 export default function GameLayout({
   children, // will be a page or nested layout
 }: {
