d0b0975495fd888f1c34ec040063b6ab40dcf7cf
rurigrass <ruripg@gmail.com>
2024-07-27 16:25:22 +0200

    ok now video height and width are limited by container size

--- src/components/game/VideoPlayer.tsx
+++ src/components/game/VideoPlayer.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { motion, spring } from "framer-motion";
 
 interface VideoPlayerProps {
@@ -9,6 +9,22 @@ interface VideoPlayerProps {
 
 const VideoPlayer = ({ fileUrl }: VideoPlayerProps) => {
   const [muted, setMuted] = useState<boolean>(true);
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  // const coverRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    console.log("CONTAINER ", containerRef.current?.offsetHeight);
+    if (containerRef.current && videoRef.current) {
+      videoRef.current.style.maxHeight = `${containerRef.current?.offsetHeight}px`;
+      videoRef.current.style.maxWidth = `${containerRef.current?.offsetWidth}px`;
+      videoRef.current.style.height = `auto`;
+      videoRef.current.style.width = `auto`;
+      // coverRef.current.style.height = `${containerRef.current.offsetHeight}px`;
+      // coverRef.current.style.width = `${containerRef.current.offsetWidth}px`;
+      console.log("hello ", videoRef.current);
+    }
+  }, []);
 
   //MOUSE HOVER STUFF:
   // ${
@@ -20,26 +36,33 @@ const VideoPlayer = ({ fileUrl }: VideoPlayerProps) => {
   //OLD DIMENSIONS max-h-[calc(60dvh)] lg:max-h-[calc(90dvh)] w-auto
   //ALSO THESE h-[60%] w-auto lg:h-[80%]
   return (
-    <div className="flex relative items-center h-full justify-center lg:justify-start rounded-lg overflow-hidden ">
-      {/* <div className="relative h-full w-full">
-        <motion.div
-          onClick={() => setMuted(!muted)}
-          key="animation-on-muted"
-          className="absolute max-w-full inset-0 m-auto rounded-lg"
-          animate={
-            muted ? { opacity: 0.5, backgroundColor: "#000" } : { opacity: 0 }
-          }
-          transition={{ type: "spring", delay: 0.2 }}
+    <div
+      ref={containerRef}
+      className="flex relative items-center justify-center h-full w-full lg:justify-center overflow-hidden"
+    >
+      {/* <div className="relative h-full w-full"> */}
+      {/* <motion.div
+        ref={coverRef}
+        onClick={() => setMuted(!muted)}
+        key="animation-on-muted"
+        className="absolute  m-auto rounded-lg"
+        animate={
+          muted ? { opacity: 0.5, backgroundColor: "#000" } : { opacity: 0 }
+        }
+        transition={{ type: "spring", delay: 0.2 }}
         >
-          <div className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 text-[4.6vw] font-bold text-white opacity-70 mix-blend-screen break-normal ">
-            Click for Sound
-          </div>
-        </motion.div> */}
+       <div className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 text-[4.6vw] font-bold text-white opacity-70 mix-blend-screen break-normal ">
+          Click for Sound
+        </div> 
+        </motion.div>  */}
+
       <video
-        className={`rounded-lg ${muted && "hover:cursor-pointer"} `}
+        ref={videoRef}
+        className={`${
+          muted && "hover:cursor-pointer"
+        } object-contain rounded-lg !important h-0 w-0`}
         autoPlay
         loop
-        //   controls
         playsInline
         muted={muted}
         onClick={() => setMuted(!muted)}
